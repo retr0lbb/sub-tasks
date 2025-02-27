d0ec165d5dc356bba42b61767fbf46566d900fdf
retr0lbb <retr0lbb@gmail.com>
2025-02-26 23:02:55 -0300

    test: update test coverage to complete task

--- src/handlers/complete-task.test.ts
+++ src/handlers/complete-task.test.ts
@@ -48,6 +48,64 @@ describe("it should complete tasks", async () => {
         await expect(toggleTaskCompletion({taskId: "major-task", completion: false})).rejects.toBeInstanceOf(ClientError)
     }) 
 
-    it("should complete all subtasks")
+    it("should return error if complete and isCompleted are equal", async() => {
+        vi.mocked(prisma.tasks.findUnique).mockResolvedValue({
+            id: 'major-task',
+            description: "random",
+            isCompleted: true,
+            parentId: null,
+            createdAt: new Date(),
+            title: "random",
+            updatedAt: null
+        })
+
+        await expect(toggleTaskCompletion({taskId: "major-task", completion: true}))
+          .rejects.toBeInstanceOf(ClientError)
+    })
+
+    it("should complete all subtasks from a task", async() => {
+        const tasks = {
+            task1: {
+                id: 'major-task',
+                description: "random",
+                isCompleted: false,
+                parentId: null,
+                createdAt: new Date(),
+                title: "random",
+                updatedAt: null
+            },
+            task2: {
+                id: 'sub-major-task',
+                description: "random",
+                isCompleted: false,
+                parentId: "major-task",
+                createdAt: new Date(),
+                title: "random",
+                updatedAt: null
+            },
+            task3: {
+                id: 'sub-major-task',
+                description: "random",
+                isCompleted: true,
+                parentId: "major-task",
+                createdAt: new Date(),
+                title: "random",
+                updatedAt: null
+            }
+        }
+
+        vi.mocked(prisma.tasks.findUnique).mockResolvedValue(tasks.task1)
+        vi.mocked(prisma.tasks.findMany).mockResolvedValueOnce([tasks.task2, tasks.task3])
+
+        await toggleTaskCompletion({taskId: tasks.task1.id, completion:  true})
+
+        expect(prisma.tasks.findMany).toBeCalledTimes(1)
+        expect(prisma.tasks.findMany).toHaveBeenCalledWith({
+            where: {
+                parentId: tasks.task1.id
+            }
+        })
+
+    })
 
 })
