fe6fa2d9b5a6f9bb5a939b24101615804e16a88c
retr0lbb <retr0lbb@gmail.com>
2025-09-11 03:29:39 -0300

    fix: add host to fastify

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,7 @@ import { env } from "./utils/env";
 app
 	.listen({
 		port: env.PORT || 3333,
+		host: "0.0.0.0",
 	})
 	.then(() => {
 		app.log.info(`Server started on http://localhost:${env.PORT}`);
